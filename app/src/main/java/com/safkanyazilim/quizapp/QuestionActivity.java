31fc7987c2ef82fb39b526920b1f628250ada6e1
Elif T. Kus <elifkus@gmail.com>
2016-06-25 15:04:26 +0300

    Enable button after a radio button is checked

--- app/src/main/java/com/safkanyazilim/quizapp/QuestionActivity.java
+++ app/src/main/java/com/safkanyazilim/quizapp/QuestionActivity.java
@@ -2,18 +2,34 @@ package com.safkanyazilim.quizapp;
 
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.RadioGroup;
 
-public class QuestionActivity extends FragmentActivity {
+public class QuestionActivity extends FragmentActivity implements RadioGroup.OnCheckedChangeListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_question);
+
+        RadioGroup choicesRadioGroup = (RadioGroup)findViewById(R.id.choicesRadioGroup);
+        choicesRadioGroup.setOnCheckedChangeListener(this);
     }
 
     public void answerQuestion(View view) {
         ResultDialogFragment newFragment = new ResultDialogFragment();
         newFragment.show(getFragmentManager(), "result");
+    }
+
+
+    @Override
+    public void onCheckedChanged(RadioGroup group, int checkedId) {
+        Button answerButton = (Button) findViewById(R.id.answerButton);
+        
+        if (checkedId != -1) {
+            answerButton.setClickable(true);
+        }
     }
 }
